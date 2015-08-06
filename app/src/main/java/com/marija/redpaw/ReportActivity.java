f36641fbc8d097a68c8dc6899710267a5ee2765e
Carolina Sartorius <carolina.sartorius@gmail.com>
2015-08-06 16:12:26 +0100

    The found button works!

--- app/src/main/java/com/marija/redpaw/ReportActivity.java
+++ app/src/main/java/com/marija/redpaw/ReportActivity.java
@@ -17,7 +17,7 @@ public class ReportActivity extends ActionBarActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_report);
     }
 
     public void onClickBtnLocate (View view) {
