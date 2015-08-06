9514198ff669a54879bef0f87afc48a99a242bf2
muumintroll <m1niSeelik>
2015-08-06 15:13:24 +0100

    Added content to animal file

--- app/src/main/java/com/marija/redpaw/Animal.java
+++ app/src/main/java/com/marija/redpaw/Animal.java
@@ -1,7 +1,48 @@
 package com.marija.redpaw;
 
+import android.graphics.Bitmap;
+import android.view.animation.AnticipateInterpolator;
+
 /**
- * Created by demouser on 8/6/15.
+ * Container class for an animal object.
  */
 public class Animal {
+    private String description;
+    private Type type;
+    private Bitmap img;
+    public enum Type{Cat, Dog, Other} ;
+
+    //Empty constructor
+    public Animal(){}
+
+    public Animal(String description, Type type, Bitmap img ){
+        this.description=description;
+        this.type=type;
+        this.img=img;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+    public void setDescription(String description){
+        this.description=description;
+    }
+
+    public Type getType(){
+        return type;
+    }
+
+    public void setType(Type type){
+        this.type=type;
+    }
+
+    public Bitmap getImg(){
+        return img;
+    }
+
+    public void setImg(Bitmap img){
+        this.img=img;
+    }
+
 }
