a82aee5ef7ec7aff634af479aa364ed350480d34
muumintroll <m1niSeelik>
2015-08-06 16:39:30 +0100

    Added adapter to AdoptActivity

--- app/src/main/java/com/marija/redpaw/AdoptActivity.java
+++ app/src/main/java/com/marija/redpaw/AdoptActivity.java
@@ -2,17 +2,32 @@ package com.marija.redpaw;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 /**
  * Created by demouser on 8/6/15.
  */
 public class AdoptActivity extends ActionBarActivity {
+    private ArrayList<Animal> animalsInShelter;
+    private ListView listView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        animalsInShelter=new ArrayList<Animal>();
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_adopt);
+        listView=(ListView)findViewById(R.id.adopt_listViewResults);
+        MyAdapter adapter=new MyAdapter();
+        listView.setAdapter(adapter);
+
     }
 
     @Override
@@ -35,5 +50,48 @@ public class AdoptActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public class MyAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return animalsInShelter.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+
+            return animalsInShelter.get(position%animalsInShelter.size());
+        }
+
+        @Override
+        public long getItemId(int position) {
+
+            return position % animalsInShelter.size() ;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View v;
+            ViewHolder vh;
+            if(convertView!=null ){
+                v=convertView;
+                vh=(ViewHolder)v.getTag();
+            }else {
+                LayoutInflater inf=LayoutInflater.from(AdoptActivity.this);
+                v= inf.inflate(R.layout.list_item, null);
+                vh=new ViewHolder();
+                v.setTag(vh);
+            }
+            return v;
+        }
+    }
+
+    /*
+    Needed to show text for each animal.
+     */
+    public class ViewHolder{
+        //No objects in view yet.
     }
 }
