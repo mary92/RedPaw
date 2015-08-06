def2bded5aa0d91e0b67a1a795629192e1bee34d
muumintroll <m1niSeelik>
2015-08-06 22:54:35 +0100

    Lots of conflicts

--- app/src/main/java/com/marija/redpaw/AdoptActivity.java
+++ app/src/main/java/com/marija/redpaw/AdoptActivity.java
@@ -43,8 +43,22 @@ public class AdoptActivity extends ActionBarActivity {
         MyAdapter listViewAdapter=new MyAdapter();
         listView.setAdapter(listViewAdapter);
 
-        // Add database event listener.
+        // Add database event listener
         referenceShelters.addValueEventListener(new DBEventListener());
+
+        // Widget Toolbar.
+        android.support.v7.widget.Toolbar actionToolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.adopt_toolbar);
+        setSupportActionBar(actionToolbar);
+        actionToolbar.setLogo(R.mipmap.ic_launcher);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        android.support.v7.widget.Toolbar actionToolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.adopt_toolbar);
+        actionToolbar.setTitle("   Red paw");
+        // Add database event listener.
+        
     }
 
     @Override
@@ -99,6 +113,7 @@ public class AdoptActivity extends ActionBarActivity {
             View view ;
             ViewHolder viewHolder;
             Pair currentPair=animalsInShelter.get(position%animalsInShelter.size());
+            // Reuse views.
             if(convertView!=null ){
                 view=convertView;
                 viewHolder=(ViewHolder)view.getTag();
@@ -126,12 +141,15 @@ public class AdoptActivity extends ActionBarActivity {
         public void onDataChange(DataSnapshot snapshot) {
             Shelter currentShelter;
             ArrayList<Shelter> tmpAnimal = new ArrayList<Shelter>((int) snapshot.getChildrenCount());
+            // Go through all of the shelters.
             for (DataSnapshot dataSnapshot1 : snapshot.getChildren()) {
                 currentShelter = dataSnapshot1.getValue(Shelter.class);
+                // Go through all of the animals in this shelter.
                 for(Animal animal:currentShelter.getAnimals()){
                     animalsInShelter.add(new Pair(animal,currentShelter));
                 }
             }
+            // Force the view to update.
             ((MyAdapter) listView.getAdapter()).notifyDataSetChanged();
         }
 
