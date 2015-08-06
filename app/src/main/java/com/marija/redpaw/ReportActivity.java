f85e5c7d369f7bdc876fac3431d357447f75df89
Carolina Sartorius <carolina.sartorius@gmail.com>
2015-08-06 21:26:37 +0100

    Refactored ReportActivity

--- app/src/main/java/com/marija/redpaw/ReportActivity.java
+++ app/src/main/java/com/marija/redpaw/ReportActivity.java
@@ -88,6 +88,29 @@ public class ReportActivity extends ActionBarActivity {
         actionToolbar.setTitle("   Red paw");
     }
 
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.menu_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
     private class AnimalAdapter extends BaseAdapter implements SpinnerAdapter{
         String[] animals = {"Cat", "Dog", "Other"};
 
@@ -235,25 +258,4 @@ public class ReportActivity extends ActionBarActivity {
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.menu_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
