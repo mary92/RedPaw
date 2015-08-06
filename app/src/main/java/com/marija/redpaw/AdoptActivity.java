fc597c1799372568f38bb611ce149903120dedbd
muumintroll <m1niSeelik>
2015-08-06 17:51:31 +0100

    Added adapter to AdoptActivity

--- app/src/main/java/com/marija/redpaw/AdoptActivity.java
+++ app/src/main/java/com/marija/redpaw/AdoptActivity.java
@@ -8,7 +8,9 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -19,14 +21,20 @@ public class AdoptActivity extends ActionBarActivity {
     private ArrayList<Animal> animalsInShelter;
     private ListView listView;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         animalsInShelter=new ArrayList<Animal>();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adopt);
+        // Get refernecse to shelters database
+        Firebase.setAndroidContext(this);
+
+        //Add adapter to list view.
         listView=(ListView)findViewById(R.id.adopt_listViewResults);
-        MyAdapter adapter=new MyAdapter();
-        listView.setAdapter(adapter);
+        MyAdapter listViewAdapter=new MyAdapter();
+        listView.setAdapter(listViewAdapter);
+
 
     }
 
@@ -73,18 +81,27 @@ public class AdoptActivity extends ActionBarActivity {
 
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
-            View v;
-            ViewHolder vh;
+            View view ;
+            ViewHolder viewHolder;
+            Animal currentAnimal=animalsInShelter.get(position%animalsInShelter.size());
             if(convertView!=null ){
-                v=convertView;
-                vh=(ViewHolder)v.getTag();
+                view=convertView;
+                viewHolder=(ViewHolder)view.getTag();
             }else {
                 LayoutInflater inf=LayoutInflater.from(AdoptActivity.this);
-                v= inf.inflate(R.layout.list_item, null);
-                vh=new ViewHolder();
-                v.setTag(vh);
+                view= inf.inflate(R.layout.list_item, null);
+                // Create links to view objects.
+                viewHolder=new ViewHolder();
+                viewHolder.img=(ImageView)findViewById(R.id.adoptitem_imgViewPhoto);
+                viewHolder.description=(TextView)findViewById(R.id.adoptitem_description);
+                viewHolder.shelter=(TextView)findViewById(R.id.adoptitem_shelter);
+                view.setTag(viewHolder);
             }
-            return v;
+            // Set values in images.
+            viewHolder.img.setImageBitmap(currentAnimal.getImg());
+            viewHolder.description.setText(currentAnimal.getDescription());
+            //viewHolder.shelter.setText();<---- Where do we get the shelter from?
+            return view;
         }
     }
 
@@ -92,6 +109,8 @@ public class AdoptActivity extends ActionBarActivity {
     Needed to show text for each animal.
      */
     public class ViewHolder{
-        //No objects in view yet.
+        public ImageView img;
+        public TextView description;
+        public TextView shelter;
     }
 }
