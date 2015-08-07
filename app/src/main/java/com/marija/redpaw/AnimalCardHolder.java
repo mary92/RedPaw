d1f090254590334ec633600b3662eb7081a389bc
Lavinia Damian <lavinia.damian27@gmail.com>
2015-08-07 01:25:12 +0100

    Merge branch 'development' of https://github.com/mary92/RedPaw into development
    # Conflicts:
    #	.idea/modules.xml
    #	app/src/main/java/com/marija/redpaw/Shelter.java
    #	app/src/main/res/layout/animal_card.xml

--- app/src/main/java/com/marija/redpaw/AnimalCardHolder.java
+++ app/src/main/java/com/marija/redpaw/AnimalCardHolder.java
@@ -7,6 +7,7 @@ import android.widget.TextView;
  * Created by demouser on 8/6/15.
  */
 public class AnimalCardHolder {
+    public int position;
     public ImageView animalPic;
     public TextView animalDescription;
 }
